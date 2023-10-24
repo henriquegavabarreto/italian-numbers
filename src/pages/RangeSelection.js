93d435188f5bd04b6db9f7d399ffa9a6b0f25f85
Henrique Gava <henriquegavabarreto@gmail.com>
2023-10-23 18:05:41 -0700

    implements simple range validation

--- src/pages/RangeSelection.js
+++ src/pages/RangeSelection.js
@@ -2,11 +2,59 @@ import NumberInput from "../components/NumberInput"
 import ActionButton from "../components/ActionButton"
 
 const RangeSelection = ({ minNumber, maxNumber, setMinNumber, setMaxNumber, setPage}) => {
+    const minInput = {
+        min: 0,
+        max: 999999999989
+    }
+
+    const maxInput = {
+        min: 10,
+        max: 999999999999
+    }
+
+    function handleClick () {
+        let isInputValid = true
+        
+        // check if minNumber is in valid range
+        if (minNumber < minInput.min || minNumber > minInput.max) {
+            console.log("Invalid min number")
+            // give visual feedback to the user
+            isInputValid = false
+        }
+
+        // check if maxNumber is in valid range
+        if (maxNumber < maxInput.min || maxNumber > maxInput.max) {
+            console.log("invalid max number")
+            // give visual feedback to the user
+            isInputValid = false
+        }
+
+        // be sure that min < max
+        if(minNumber > maxNumber) {
+            console.log("Max number should be greater than min number")
+            // give visual feedback to the user
+            isInputValid = false
+        }
+
+        if (!isInputValid) return
+
+        setPage("guess")
+    }
     return (
         <>
-        <NumberInput name="Min" min="0" max="999999999989" value={minNumber} setValue={(value) => setMinNumber(parseInt(value))}/>
-        <NumberInput name="Max" min="10" max="999999999999" value={maxNumber} setValue={(value) => setMaxNumber(parseInt(value))}/>
-        <ActionButton text=">>" action={() => setPage("guess")}/>
+        <NumberInput
+            name="Min"
+            min={minInput.min}
+            max={minInput.max}
+            value={minNumber}
+            setValue={(value) => setMinNumber(parseInt(value))}/>
+        <NumberInput
+            name="Max"
+            min={maxInput.min}
+            max={maxInput.max}
+            value={maxNumber}
+            setValue={(value) => setMaxNumber(parseInt(value))}/>
+        <ActionButton text=">>" action={handleClick}/>
         </>
     )
 }
